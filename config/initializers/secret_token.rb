d24e75e04bb6b14a0b1cba73d947885cb4ff7e62
Thomas Cioppettini <thomas.cioppettini@gmail.com>
2012-01-08 23:10:24 -0500

    Generated a new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketTracker::Application.config.secret_token = 'e93dfded8c3dc2bb2cd174551dee9150f25c6c558f9366affe7125923745731160164af8a5c7d87cea80c395ffc64b4934dd548040d653eebe17f60bfead8cc9'
