482a51b85e9e9165a38bc1e3667411eaafa23274
Thomas Cioppettini <thomas.cioppettini@gmail.com>
2012-01-12 16:23:33 -0500

    Fix signing up feature, take user to alternate confirmation page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 TicketTracker::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
+  get 'awaiting_confirmation',
+    :to => "users#confirmation",
+    :as => 'confirm_user'
 
   root :to => "Projects#index"
   
