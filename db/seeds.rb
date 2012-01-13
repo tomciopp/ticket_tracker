6591d45acba3ff6e4e089cb4313be8bb1f312ef9
Thomas Cioppettini <thomas.cioppettini@gmail.com>
2012-01-13 16:38:48 -0500

    Added a seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+admin_user = User.create!(:email => "admin@ticketee.com", :password => "password")
+
+admin_user.admin = true
+admin_user.confirm!
+
+Project.create(:name => "Ticketee Beta")
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
